ea6b6219b7cecd0ce61ad52e583cba60f74bf0d6
Pranitha <pranitha0090@gmail.com>
2025-07-30 12:54:58 -0500

    Added Dice printing side by side logic

--- src/main/java/diceRollingSimulator/Simulator.java
+++ src/main/java/diceRollingSimulator/Simulator.java
@@ -1,43 +1,92 @@
 package diceRollingSimulator;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Simulator {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         Random rand = new Random();
-        boolean appCompleted=false;
+        boolean appCompleted = false;
         do {
             try {
                 System.out.println("how many dice do you want to roll");
                 int numberOfDice = scanner.nextInt();
-                appCompleted=true;
+                appCompleted = true;
+                // 1. Store the roll values first
+                ArrayList<Integer> rolls = new ArrayList<>();
                 for (int i = 0; i < numberOfDice; i++) {
-                    int value = rand.nextInt(6) + 1;
-                    System.out.println(display(value));
+                    rolls.add(rand.nextInt(6) + 1);
                 }
 
-            }
-            catch (InputMismatchException e){
+                // 2. Print line by line
+                // Each die has 5 lines of text
+                for (int line = 0; line < 5; line++) {
+                    // Loop through each roll for the current line
+                    for (int rollValue : rolls) {
+                        // Get the array of lines for the current die
+                        String[] dieLines = display(rollValue);
+                        // Print the correct line for this die, without a newline
+                        System.out.print(dieLines[line] + "  ");
+                    }
+                    // After printing the line for all dice, go to the next line
+                    System.out.println();
+                    for (int i = 0; i < numberOfDice; i++) {
+                        int value = rand.nextInt(6) + 1;
+                        System.out.println(display(value));
+                    }
+
+                }
+            } catch (InputMismatchException e) {
                 System.out.println("please enter a valid number");
                 scanner.next();
             }
         }
 
-        while(!appCompleted);
+            while (!appCompleted) ;
 
-    }
-    public static String display(int value){
-        return switch (value){
-            case 1->"-------\n|     |\n|  0  |\n|     |\n-------";
-            case 2-> "-------\n| 0   |\n|     |\n|   0 |\n-------";
-            case 3-> "-------\n| 0   |\n|  0  |\n|   0 |\n-------";
-            case 4-> "-------\n| 0 0 |\n|     |\n| 0 0 |\n-------";
-            case 5-> "-------\n| 0 0 |\n|  0  |\n| 0 0 |\n-------";
-            case 6-> "-------\n| 0 0 |\n| 0 0 |\n| 0 0 |\n-------";
-            default-> "not a valid die value";
-        };
-    }
+        }
+        public static String[] display ( int value){
+            return switch (value) {
+                case 1 -> new String[]{
+                        "---------",
+                        "|       |",
+                        "|   O   |",
+                        "|       |",
+                        "---------"};
+                case 2 -> new String[]{
+                        "---------",
+                        "| O     |",
+                        "|       |",
+                        "|     O |",
+                        "---------"};
+                case 3 -> new String[]{
+                        "---------",
+                        "| O     |",
+                        "|   O   |",
+                        "|     O |",
+                        "---------"};
+                case 4 -> new String[]{
+                        "---------",
+                        "| O   O |",
+                        "|       |",
+                        "| O   O |",
+                        "---------"};
+                case 5 -> new String[]{
+                        "---------",
+                        "| O   O |",
+                        "|   O   |",
+                        "| O   O |",
+                        "---------"};
+                case 6 -> new String[]{
+                        "---------",
+                        "| O   O |",
+                        "| O   O |",
+                        "| O   O |",
+                        "---------"};
+                default -> new String[]{"invalid"};
+            };
+        }
 }
